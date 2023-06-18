cfa5d6762061809c859e5ce6a47609724e5220c2
Antonio0512 <antonioboyanov9@gmail.com>
2023-06-18 21:43:38 +0300

    Changes to the allowed hosts.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_integer" "ri" {
-  min = 10000
-  max = 99999
-}
-
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_group_name}${random_integer.ri.result}"
